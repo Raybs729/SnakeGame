259d14468ad934a948ec4fb7525f8845b2ade374
dev global <raybriones729@gmail.com>
2023-05-27 13:54:02 -0500

    committing gamepanel updates

--- src/GameFrame.java
+++ src/GameFrame.java
@@ -1,4 +1,16 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GameFrame extends JFrame {
+    GameFrame() {
+
+        this.add(new GamePanel());
+        this.setTitle("Snake");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack(); //pack will take jframe and fit it around the components that we add to frame
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
 }
--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,12 +1,59 @@
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.Random;
+
 
 public class GamePanel extends JPanel implements ActionListener {
 
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE; // this is each individual space for snake to move on game screen.
+    static final int DELAY = 75; // the higher the number the slower the game is
+    final int x[] = new int[GAME_UNITS]; //snake will not be bigger than total game units (game itself)
+    final int y[] = new int[GAME_UNITS]; //this holds y coordinates while x holds x coordinates
+    int bodyParts = 6;
+    int applesEaten; //
+    int appleX; //x coordinate where apple is located. will appear randomly
+    int appleY; //y position of apple
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+    public GamePanel(){
+
+    }
+    public void startGame(){
+
+    }
+    public void paintComponent(Graphics g){
+
+    }
+    public void draw (Graphics g){
+
+    }
+    public void move(){
+
+    }
+    public void checkApple(){
+
+    }
+    public void checkCollisions(){
+
+    }
+    public void gameOver(Graphics g){
+
+    }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         
+    }
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
     }
 }
